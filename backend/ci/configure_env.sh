0de6cb1bb97f3473ad6e0ddd3105b57cade7e4e0
prasad-sawantdesai <prasad.sawantdesai@gmail.com>
2026-05-22 14:51:41 +0200

    pin version of IMAS-Core and Python

--- backend/ci/configure_env.sh
+++ backend/ci/configure_env.sh
@@ -7,8 +7,8 @@ source /etc/profile.d/modules.sh
 module purge
 
 # Set up environment
-module load Python
-module load IMAS-Core
+module load Python/3.11.5-GCCcore-13.2.0
+module load IMAS-Core/5.6.0-intel-2023b
 
 # Debuggging:
 echo "Done loading modules"
